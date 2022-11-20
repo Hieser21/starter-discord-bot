dc14c3c0c86ae8167f9a55c7fdc0fb4f166d1c7c
Hieser21 <96245505+Hieser21@users.noreply.github.com>
2022-11-20 14:31:56 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const APPLICATION_ID = process.env.APPLICATION_ID 
 const TOKEN = process.env.TOKEN 
 const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
-const GUILD_ID = process.env.GUILD_ID 
+const GUILD_ID = null
 
 
 const axios = require('axios')
